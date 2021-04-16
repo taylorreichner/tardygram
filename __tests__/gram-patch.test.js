eb810c05a0332c6bf7fda9ae529820a7241aaa2a
SamuelHGerber <henrylightfoot@gmail.com>
2021-04-16 15:45:45 -0700

    test failing for GET /grams

--- __tests__/gram-patch.test.js
+++ __tests__/gram-patch.test.js
@@ -3,16 +3,16 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Gram = require('../lib/models/Gram');
-const User = require('../lib/models/User')
+const User = require('../lib/models/User');
 
 jest.mock('../lib/middleware/ensureAuth.js', () => (req, res, next) => {
-    req.user = {
-      username: 'test_user',
-      photoUrl: 'http://image.com/image.png',
-    };
-  
-    next();
-  });
+	req.user = {
+		username: 'test_user',
+		photoUrl: 'http://image.com/image.png',
+	};
+
+	next();
+});
 
 describe('gram routes', () => {
 	beforeEach(() => {
@@ -21,46 +21,51 @@ describe('gram routes', () => {
 
 	it('creates a gram via POST', async () => {
 		await User.insert({
-		  username: 'test_user',
-		  photoUrl: 'http://image.com/image.png',
+			username: 'test_user',
+			photoUrl: 'http://image.com/image.png',
 		});
-	
-		return request(app)
-		  .post('/api/v1/grams/new')
-		  .send({ 
-			  caption: 'Hello this is some text',
-			  tags: ['blessed'],
-			  author: 1,
-			  photoUrl: 'yo',
-		   })
-		  .then((res) => {
-			expect(res.body).toEqual({
-			  id: '3',
-			  caption: 'Hello this is some text',
-			  tags: ['blessed'],
-			  author: '1',
-			  photoUrl: 'yo',
-			  
-			});
-		  });
-	  });
 
-	// beforeEach(async () => {
-	// 	//User.insert
-	// 	const gramOne = await Gram.insert({
-	// 		photoUrl: 'https://bit.ly/fcc-relaxing-cat',
-	// 		caption: 'He just woke up',
-	// 		tags: ['yes', 'no', 'maybe'],
-	// 		author: 'melvin',
-	// 	});
-	// 	await Gram.insert({
-	// 		photoUrl: 'https://bit.ly/fcc-relaxing-cat',
-	// 		caption: '',
-	// 		tags: ['yes', 'no', 'maybe'],
-	// 		author: 'sarah',
-	// 	});
-	// 	console.log(gramOne);
-	// });
+		return request(app)
+			.post('/api/v1/grams/new')
+			.send({
+				caption: 'Hello this is some text',
+				tags: ['blessed'],
+				author: 1,
+				photoUrl: 'yo',
+			})
+			.then((res) => {
+				expect(res.body).toEqual({
+					id: '3',
+					caption: 'Hello this is some text',
+					tags: ['blessed'],
+					author: '1',
+					photoUrl: 'yo',
+				});
+			});
+	});
+
+	it('gets all grams', () => {
+		return request(app)
+			.get('/api/v1/grams')
+			.then((res) => {
+				expect(res.body).toEqual([
+					{
+						id: '1',
+						caption: 'this is a caption',
+						tags: ['tag1', 'tag2', 'tag3'],
+						author: '1',
+						photoUrl: 'gram_url',
+					},
+					{
+						id: '2',
+						caption: 'this is another caption',
+						tags: ['tag1', 'tag2', 'tag3'],
+						author: '2',
+						photoUrl: 'gram_url',
+					},
+				]);
+			});
+	});
 
 	it('updates a gram by id', () => {
 		return request(app)
@@ -73,30 +78,23 @@ describe('gram routes', () => {
 					id: '2',
 					photoUrl: 'gram_url',
 					caption: 'We had a great time in Denmark',
-					tags: [
-						"tag1",
-        			    "tag2",
-      				    "tag3",
-						],
+					tags: ['tag1', 'tag2', 'tag3'],
 					author: '2',
 				});
 			});
 	});
 
-	   it('deletes a gram by id', () => {
-	     return request(app)
-	      .delete('/api/v1/grams/1')
-	      .then((res) => {
-	        expect(res.body).toEqual({
-			  id: '1',
-	          photoUrl: 'gram_url',
-	          caption: 'this is a caption',
-	          tags: ['tag1', 'tag2', 'tag3'],
-	          author: '1',
-	        });
-	      });
-	  });
-
-
-
+	it('deletes a gram by id', () => {
+		return request(app)
+			.delete('/api/v1/grams/1')
+			.then((res) => {
+				expect(res.body).toEqual({
+					id: '1',
+					photoUrl: 'gram_url',
+					caption: 'this is a caption',
+					tags: ['tag1', 'tag2', 'tag3'],
+					author: '1',
+				});
+			});
+	});
 });
